9e4327f4b7288be61d21fd6759b9e80999ad4b8f
Ankit Personal <chaudharyankit2022@gmail.com>
2026-02-17 22:15:49 +0530

    Temporal dates

---
+++ 10 Temporal Dates/01-Intro.js
@@ -0,0 +1,16 @@
+/*
+Temporal is the new standard for date and time in JavaScript.
+New Temporal objects are designed to replace the old Date object.
+Unlike legacy Date, Temporal objects are immutable and provide first-class support for time zones and non-Gregorian calendars.
+
+Temporal separates date and time into distinct classes to prevent "wall-clock" errors.
+
+Main Temporal Objects
+Temporal.Now	         -> The current time
+Temporal.ZonedDateTime	 -> Date and time in a specific time zone
+Temporal.Instant	     -> A fixed point in time, independent of time zone
+*/
+
+//Temporal.now
+const now = Temporal.Now.zonedDateTimeISO();
+console.log(now)
