730118d53371064be108640266cdbf1ece72fd68
Ankit Personal <chaudharyankit2022@gmail.com>
2026-02-28 19:59:27 +0530

    sets complete

--- 12 Sets/03-logic.js
+++ 12 Sets/03-logic.js
@@ -3,3 +3,15 @@ const B = new Set(['b', 'c', 'd']);
 
 const C = A.union(B);
 console.log(C) //a, b, c,d
+
+const D = A.intersection(B);
+console.log(D) //b, c  //COMMON
+
+const E = A.difference(B);
+console.log(E) //a  //IN A NOT IN B
+
+const F = new Set(['a', 'b']);
+const G = new Set(['a', 'b', 'c', 'd']);
+
+console.log(F.isSubsetOf(G)) //true
+console.log(G.isSupersetOf(F)) //true
