c5b362ba1f818b1f8085b7dda6edf63306455367
Ankit Personal <chaudharyankit2022@gmail.com>
2026-03-05 19:39:42 +0530

    js math

---
+++ 15 Maths/index.js
@@ -0,0 +1,7 @@
+console.log(Math.PI) //Returns the value of PI
+console.log(Math.E) //Returns the value of Euler's number
+console.log(Math.SQRT2) //Returns the square root of 2
+console.log(Math.SQRT1_2) //Returns the square root of 1/2
+console.log(Math.abs(-5)) //Returns the absolute value of a number
+console.log(Math.ceil(4.2)) //Returns the smallest integer greater than or equal to a number
+console.log(Math.floor(4.7)) //Returns the largest integer less than or equal to a number
