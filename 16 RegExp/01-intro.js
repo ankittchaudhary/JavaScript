f82d4806a0b12be65ffdbd42c8bbb9a199670b51
Ankit Personal <chaudharyankit2022@gmail.com>
2026-03-06 22:32:20 +0530

    regex intro

---
+++ 16 RegExp/01-intro.js
@@ -0,0 +1,14 @@
+/*
+Regular Expressions
+====================
+A Regular Expression is a sequence of characters that forms a search pattern.
+Regex is a common shorthand for a regular expression.
+
+JavaScript RegExp is an Object for handling Regular Expressions.
+
+RegExp are be used for:
+
+ - Text searching
+ - Text replacing
+ - Text validation
+*/
