7ec98d297e38558c0dbb80bc4f1a161b49e8535f
Ankit Personal <chaudharyankit2022@gmail.com>
2026-03-07 00:05:37 +0530

    Form Validator

--- 16 RegExp/01-intro.js
+++ 16 RegExp/01-intro.js
@@ -11,4 +11,25 @@ RegExp are be used for:
  - Text searching
  - Text replacing
  - Text validation
+
+Regular Expressions (Regex) in JavaScript are used to search, match, validate, and replace text patterns. 
+They are extremely useful in form validation, log parsing, search features, and data cleaning.
+*/
+
+let text = "Hello World";
+let pattern = /World/i; // Regular Expression pattern with 'i' flag for case-insensitive search
+
+/*
+/world/ is a regular expression.
+world is the pattern we want to search for in the text.
+
+*/
+
+console.log(pattern.test(text)); // Output: true
+
+/*
+Syntax of Regular Expressions:
+/pattern/modifier flags;
+
+
 */
